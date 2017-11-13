2333f8af70807a71b66cbe62fa3b0a6032902713
Jacques <s-jacques@live.com>
2017-11-12 21:29:43 -0500

    Final

--- HTTPServer.py
+++ HTTPServer.py
@@ -1,15 +1,19 @@
 import socket
 import threading
 import argparse
-from os import listdir
+import os
+from os import path
 
 
 def run_server(host, port):
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     try:
         listener.bind((host, port))
         listener.listen(5)
+
         print('Echo server is listening at', port)
+
         while True:
             conn, addr = listener.accept()
             threading.Thread(target=handle_client, args=(conn, addr)).start()
@@ -23,56 +27,98 @@ def handle_client(conn, addr):
     try:
         while True:
             data = conn.recv(1024)
+
             if not data:
                 break
 
             request = data.decode("utf-8")
-            splitter = request.split(" ")
-            vb_splitter = request.split("\r\n")
+            req_splitter = request.split(" ")
+            body_splitter = request.split("\r\n\r\n")
+            vb_req_splitter = body_splitter[0].split("\r\n")
+
+            print(body_splitter)
+
             verbose_answer = ""
 
-            for line in vb_splitter:
+            for line in vb_req_splitter:
                 verbose_answer += str(line) + "<br>"
 
-            if "GET" in splitter:
-                get_index = splitter.index("GET")
+            if "GET" in req_splitter:
+                get_index = req_splitter.index("GET")
 
-                if splitter[get_index + 1] == "/":
+                if req_splitter[get_index + 1] == "/" or req_splitter[get_index + 1] == "/favicon.ico":
+                    temp_response = verbose_answer + "<br><br>Files Available:<br><br>"
+                    files = os.listdir('./files')
+
+                    for file in files:
+                        temp_response += "/" + file + "<br>"
 
                     response = "HTTP/1.1 200 OK\r\n"\
-                               + "Content-Type: text/html\r\n\r\n"\
-                               + verbose_answer + "\r\n"\
-                               + "Files Available:<br><br>"
-
-                    files = listdir('./files')
-                    for file in files:
-                        response += "/" + file + "<br>"
+                               + "Content-Type: text/html\r\n"\
+                               + "Content-Length: "\
+                               + str(len(temp_response))\
+                               + "\r\n\r\n"\
+                               + temp_response
                 else:
                     try:
-                        file = open("./files" + splitter[get_index + 1])
-                        response = "HTTP/1.1 200 OK\r\n"\
-                                   + "Content-Type: text/html\r\n\r\n"\
-                                   + file.read()
-                    except FileNotFoundError:
-                        response = "HTTP/1.1 404 NOT FOUND\r\n"\
-                                   + "Content-Type: text/html\r\n\r\n"\
-                                   + "HTTP ERROR 404 NOT FOUND"
-            elif "POST" in splitter:
-                response = "HTTP/1.1 200 OK\r\n"\
-                           + "Content-Type: text/html"\
-                           + "Content-Length: 16\r\n\r\n"\
-                           + "<html><body><p>File Overwritten</p></body></html>"
+                        file = open("./files" + req_splitter[get_index + 1], "r")
+                        temp_response = verbose_answer + "<br><br>" + file.read()
 
+                        response = "HTTP/1.1 200 OK\r\n" \
+                                   + "Content-Type: text/html\r\n" \
+                                   + "Content-Length: " \
+                                   + str(len(temp_response)) \
+                                   + "\r\n\r\n" \
+                                   + temp_response
+                    except FileNotFoundError:
+                        response = "HTTP/1.1 400 BAD REQUEST\r\n"\
+                                   + "Content-Type: text/html\r\n"\
+                                   + "Content-Length: 26\r\n\r\n"\
+                                   + "HTTP ERROR 400 BAD REQUEST"
+            elif "POST" in req_splitter:
+                post_index = req_splitter.index("POST")
+                path_to_data = "./files" + req_splitter[post_index + 1]
+
+                if req_splitter[post_index + 1] == "/":
+                    response = "HTTP/1.1 400 BAD REQUEST\r\n"\
+                               + "Content-Type: text/html\r\n"\
+                               + "Content-Length: 26\r\n\r\n"\
+                               + "HTTP ERROR 400 BAD REQUEST"
+                else:
+                    path_splitter = path_to_data.split("/")
+
+                    for x in range(0, len(path_splitter) - 1):
+                        new_dir = path_splitter[x] + "/"
+
+                    if not os.path.exists(new_dir):
+                        os.makedirs("./files/" + new_dir)
+
+                    if os.path.isfile(path_to_data):
+                        temp_response = verbose_answer + "<br><br>File " + req_splitter[post_index + 1] + " overwritten"
+                    else:
+                        temp_response = verbose_answer + "<br><br>File " + req_splitter[post_index + 1] + " created"
+
+                    file = open(path_to_data, "w")
+                    file.write(body_splitter[1])
+                    file.close()
+
+                    response = "HTTP/1.1 200 OK\r\n" \
+                               + "Content-Type: text/html\r\n" \
+                               + "Content-Length: " \
+                               + str(len(temp_response)) \
+                               + "\r\n\r\n" \
+                               + temp_response
             else:
                 response = "HTTP/1.1 400 BAD REQUEST\r\n"\
-                           + "Content-Type: text/html\r\n\r\n"\
+                           + "Content-Type: text/html\r\n"\
+                           + "Content-Length: 26\r\n\r\n"\
                            + "HTTP ERROR 400 BAD REQUEST"
 
             print(response)
 
             encoded_response = response.encode("utf-8")
 
-            conn.send(encoded_response)
+            conn.sendall(encoded_response)
     finally:
         conn.close()
 
